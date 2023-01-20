1fc23e5bec259e8aea8a664c68c62b18d12ad51a
dl00lb <122277027+dl00lb@users.noreply.github.com>
2023-01-20 12:09:41 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Create cache matrix from input numeric list values
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x) {
+    ## Arg x can either be square matrix or randomly generated 
+    ## nxn square matrix using the following code:
+    ## "x <- matrix((runif(n^2, min=5, max=99), nrow=n))"
 
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        
+        return list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+
+## calc inv of the vector 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+    ## if x matrix is not sq matrix, solve function will give error!!
+
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinv(m)
+        m
 }
